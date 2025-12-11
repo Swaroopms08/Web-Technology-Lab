81da59c3a02930ee7104ac52b1f03093efff7ae2
Swaroopms08 <swaroopms739@gmail.com>
2025-12-11 19:46:24 +0530

    Delete Assignment directory

--- Assignment/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll(".nav-links a");
-    links.forEach(a => {
-        if (window.location.href.includes(a.getAttribute("href"))) {
-            a.classList.add("active");
-        }
-    });
-});
